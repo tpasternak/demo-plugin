0e14658ca6b2b288c20e834472b7e55528aa588c
Tomasz Pasternak <1066003+tpasternak@users.noreply.github.com>
2022-07-15 13:16:56 +0000

    init

---
+++ src/main/kotlin/com/example/demo/DemoAction.kt
@@ -0,0 +1,10 @@
+package com.example.demo
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class DemoAction : AnAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+        TODO("Not yet implemented")
+    }
+}
